367dd823540db3d2e433d7b6eb71fac0b80c249e
ghalibansari1994 <ghalibansari1994@gmail.com>
2018-10-15 15:35:18 +0530

    .

--- index.php
+++ index.php
@@ -34,4 +34,26 @@
     </div>
   </div>
 
+
+<?php
+require_once('db.php');
+//$sql="select product_price from product";
+$sql="SELECT * FROM product WHERE product_price =  ( SELECT MIN(product_price) FROM product )";
+$result = $conn->query($sql);
+
+?>
+
+  <br>
+  <div class="ui one column stackable grid container">
+    <div class="column">
+      <div class="ui center aligned segment">
+        <h1><?php while ($row = $result->fetch_assoc()) {
+  echo $row['username'];
+  echo "  : ";
+  echo $row['product_price']."<br>";
+  
+}  ?></h1>
+      </div>
+    </div>
+
 <?php require_once('footer.php') ?>
